cf5489ff6c58a5f339609db709a20fd947d7da00
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-06-27 23:03:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,12 @@
-def hello(collection)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i+=1
-  end
-  collection
-end
 
+
+def my_collect(array)
+ 
+i=0
+collection =[]
+while i < array.length
+collection << yield(array[i]) 
+i +=1
+end
+collection
+end
